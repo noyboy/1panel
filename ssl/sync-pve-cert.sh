3f10533001e8fa9045c5ef8d45ae70e3e35f04ca
noyboy <inowboy@outlook.com>
2025-06-18 14:38:58 +0000

    增加远程主机用户名

--- ssl/sync-pve-cert.sh
+++ ssl/sync-pve-cert.sh
@@ -5,11 +5,11 @@
 #        如：sync-pve-cert.sh '/path/to/cert'
 # 要求：需要搭配ssh免密登录pve主机；
 
-# 远程pve主机IP，这里代理内网主机192.168.31.254:22到本机127.0.0.1:28122
-PVE_IP=127.0.0.1
+# 远程pve主机IP和登录用户名，这里代理内网主机192.168.31.254:22到本机127.0.0.1:28122
+HOST_USER_IP=127.0.0.1
 
 # 远程pve主机ssh端口号
-PVE_SSH_PORT=28122
+HOST_PORT=28122
 
 # 推送证书到pve主机
 # 语法：scp -P 端口号 文件路径 用户名@远程主机IP:目标目录
@@ -18,10 +18,10 @@ PVE_SSH_PORT=28122
 #    如果需要传输的是目录，可以使用-r参数来递归复制整个目录。
 # 注意需要把密钥后缀.pem改为.key
 
-scp -P ${PVE_SSH_PORT} $1/fullchain.pem root@${PVE_IP}:/etc/pve/nodes/n100/pveproxy-ssl.pem
-scp -P ${PVE_SSH_PORT} $1/privkey.pem root@${PVE_IP}:/etc/pve/nodes/n100/pveproxy-ssl.key
+scp -P ${HOST_PORT} $1/fullchain.pem ${HOST_USER_IP}:/etc/pve/nodes/n100/pveproxy-ssl.pem
+scp -P ${HOST_PORT} $1/privkey.pem ${HOST_USER_IP}:/etc/pve/nodes/n100/pveproxy-ssl.key
 
 # 重启服务，经测试会自动刷新
-# ssh  -p ${PVE_SSH_PORT} root@${PVE_IP} "systemctl restart pveproxy;"
+# ssh  -p ${HOST_PORT} root@${HOST_USER_IP} "systemctl restart pveproxy;"
 
 #END
